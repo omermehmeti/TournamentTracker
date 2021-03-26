491184f7a2fc74b4b562b589f0b2b5fcb1375e3c
Omer Mehmeti <58736018+omermehmeti@users.noreply.github.com>
2021-03-26 20:23:42 +0100

    Adding data saving functions
     Added  functions for saving prizes ,tournaments , players and teams

---
+++ TournamentUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TournamentUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // use the Class Library namspace cause happened a error during firts build
+            TTracker.GlobalConfig.InitializeConnections1(TTracker.DatabaseType.MySql);
+           
+            
+           
+            
+            Application.Run(new CreateTournamentForm());
+        }
+    }
+}
---
+++ TournamentUI/TournamentDashboardForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TournamentUI
+{
+    partial class TournamentDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TournamentDashboardForm));
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.SelectTournaments = new System.Windows.Forms.ComboBox();
+            this.LoadExistingTournamentLabel = new System.Windows.Forms.Label();
+            this.LoadTournamentButton = new System.Windows.Forms.Button();
+            this.CreateTournamentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.HeaderLabel.Location = new System.Drawing.Point(193, 25);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(238, 25);
+            this.HeaderLabel.TabIndex = 13;
+            this.HeaderLabel.Text = "Tournament Dashboard";
+            // 
+            // SelectTournaments
+            // 
+            this.SelectTournaments.FormattingEnabled = true;
+            this.SelectTournaments.Location = new System.Drawing.Point(200, 115);
+            this.SelectTournaments.Name = "SelectTournaments";
+            this.SelectTournaments.Size = new System.Drawing.Size(225, 21);
+            this.SelectTournaments.TabIndex = 17;
+            // 
+            // LoadExistingTournamentLabel
+            // 
+            this.LoadExistingTournamentLabel.AutoSize = true;
+            this.LoadExistingTournamentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadExistingTournamentLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.LoadExistingTournamentLabel.Location = new System.Drawing.Point(230, 73);
+            this.LoadExistingTournamentLabel.Name = "LoadExistingTournamentLabel";
+            this.LoadExistingTournamentLabel.Size = new System.Drawing.Size(165, 24);
+            this.LoadExistingTournamentLabel.TabIndex = 16;
+            this.LoadExistingTournamentLabel.Text = "Load Tournament:";
+            // 
+            // LoadTournamentButton
+            // 
+            this.LoadTournamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.LoadTournamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.LoadTournamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.LoadTournamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadTournamentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadTournamentButton.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LoadTournamentButton.Location = new System.Drawing.Point(236, 154);
+            this.LoadTournamentButton.Name = "LoadTournamentButton";
+            this.LoadTournamentButton.Size = new System.Drawing.Size(152, 32);
+            this.LoadTournamentButton.TabIndex = 18;
+            this.LoadTournamentButton.Text = "Load Tournament";
+            this.LoadTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // CreateTournamentButton
+            // 
+            this.CreateTournamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.CreateTournamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.CreateTournamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.CreateTournamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateTournamentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateTournamentButton.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.CreateTournamentButton.Location = new System.Drawing.Point(185, 226);
+            this.CreateTournamentButton.Name = "CreateTournamentButton";
+            this.CreateTournamentButton.Size = new System.Drawing.Size(255, 44);
+            this.CreateTournamentButton.TabIndex = 27;
+            this.CreateTournamentButton.Text = "Create Tournament";
+            this.CreateTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // TournamentDashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(604, 312);
+            this.Controls.Add(this.CreateTournamentButton);
+            this.Controls.Add(this.LoadTournamentButton);
+            this.Controls.Add(this.SelectTournaments);
+            this.Controls.Add(this.LoadExistingTournamentLabel);
+            this.Controls.Add(this.HeaderLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TournamentDashboardForm";
+            this.Text = "Tournaments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.ComboBox SelectTournaments;
+        private System.Windows.Forms.Label LoadExistingTournamentLabel;
+        private System.Windows.Forms.Button LoadTournamentButton;
+        private System.Windows.Forms.Button CreateTournamentButton;
+    }
+}
